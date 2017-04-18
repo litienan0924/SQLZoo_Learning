9273b91472de01a358a07a2ceb58315769e017e4
Tienan Li <tienanli0924@gmail.com>
2017-04-17 22:33:22 -0700

    Update 06_The_JOIN_Operation.sql

--- 06_The_JOIN_Operation.sql
+++ 06_The_JOIN_Operation.sql
@@ -1,1 +1,25 @@
+--# 1. Show the matchid and player name for all goals scored by Germany. 
+SELECT matchid, player
+FROM goal
+WHERE teamid = 'GER'
 
+--# 2. Show the id, stadium, team1, team2 for just game 1012
+SELECT id, stadium, team1, team2
+FROM game
+WHERE ID = 1012
+
+--# 3. Show the player, teamid, stadium and mdate and for every German goal
+SELECT player, teamid, stadium, mdate
+FROM game JOIN goal ON (game.id=goal.matchid)
+WHERE teamid = 'GER'
+
+--# 4. Show the team1, team2 and player for every goal scored by a player 
+--#    called Mario
+SELECT team1, team2, player
+FROM game JOIN goal ON (game.id=goal.matchid)
+WHERE player LIKE 'Mario%'
+
+--# 5. Show player, teamid, coach, gtim for all goals scored in the first 10 minutes
+SELECT player, teamid, coach, gtime
+FROM goal JOIN eteam ON (goal.teamid = eteam.id)
+WHERE gtime <= 10
